4a7523b8fd96253a82f90086a27b681873a0fbc1
Stephanie La Raja <stephanielaraja@gmail.com>
2021-03-30 08:28:17 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Stephanie"
+    end 
+
+    get '/hometown' do
+        "My hometown is NYC"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is Shake it Off"
+    end 
+
 end
